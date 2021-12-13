3d1b5e5f4cec4fcc9b573f774ac64286867e7ae2
vestigialcode <dibyajiban232@gmail.com>
2021-12-13 20:50:32 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React,{useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  
   const [state,setState]=useState("")
+  const [pika,setPika]= useState("")
+  
+  const getData=()=>{
+
+    axios.get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json')
+    .then((evt)=>{setPika(evt.data[5].name);
+    
+  
+    })
+  
+  }
   const clickHandler=()=>{
 if(state === ""){
   setState("Hello There.. how you doing")
@@ -14,6 +27,10 @@ if(state === ""){
     <div className="App">
      <p>{state}</p>
      <button  onMouseMoveCapture={clickHandler}>come here</button> 
+<p>{pika}</p>
+<image href='https://s3-ap-southeast-1.amazonaws.com/he-public-data/Richard%20Mathew3350914.jpg'></image>
+
+<button onClick={getData}>get data</button>
     </div>
   );
 }
